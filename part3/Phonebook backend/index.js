c52f108525ed1478ffb76df35aab51394c0780fb
HorusLiang <yongqing.liang@outlook.com>
2022-12-31 06:02:35 +0200

    3.1: Phonebook backend step1

---
+++ part3/Phonebook backend/index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let notes = 
+[
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+    },
+    { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+    },
+    { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+    },
+    { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/peoples', (request, response) => {
+    response.json(notes)
+})
+
+// app.get('/', (request, response) => {
+//   response.send('<h1>Hello World!</h1>')
+// })
+
+
+
+// app.get('/api/notes/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     const note = notes.find(note => note.id === id)
+    
+//     if (note) {
+//       response.json(note)
+//     } else {
+//       response.status(404).end()
+//     }
+// })
+
+
+// app.delete('/api/notes/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     notes = notes.filter(note => note.id !== id)
+
+//     response.status(204).end()
+// })
+
+// const generateId = () => {
+//     const maxId = notes.length > 0
+//       ? Math.max(...notes.map(n => n.id))
+//       : 0
+//     return maxId + 1
+//   }
+  
+//   app.post('/api/notes', (request, response) => {
+//     const body = request.body
+  
+//     if (!body.content) {
+//       return response.status(400).json({ 
+//         error: 'content missing' 
+//       })
+//     }
+  
+//     const note = {
+//       content: body.content,
+//       important: body.important || false,
+//       date: new Date(),
+//       id: generateId(),
+//     }
+  
+//     notes = notes.concat(note)
+  
+//     response.json(note)
+//   })
+  
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
