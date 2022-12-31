2a57d22f72303d0f9b88c8eda76f824eb598322f
HorusLiang <yongqing.liang@outlook.com>
2022-12-31 06:19:25 +0200

    3.3: Phonebook backend step3

--- part3/Phonebook backend/index.js
+++ part3/Phonebook backend/index.js
@@ -37,21 +37,22 @@ app.get('/info', (request, response) => {
 })
 
 
+
+
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+})
+
 // app.get('/', (request, response) => {
 //   response.send('<h1>Hello World!</h1>')
-// })
-
-
-
-// app.get('/api/notes/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     const note = notes.find(note => note.id === id)
-    
-//     if (note) {
-//       response.json(note)
-//     } else {
-//       response.status(404).end()
-//     }
 // })
 
 
