c25435269646133b83c55b7589cb5fcaf0aacbfd
HorusLiang <yongqing.liang@outlook.com>
2023-01-11 13:25:35 +0100

    4.4: helper functions and unit tests, step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,17 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes=(blogs)=>{
+    blog_num=blogs.length
+    let total = 0;
+    for (let i = 0; i < blog_num; i++) {
+        total += blogs[i].likes;
+    }
+    return total;
+}
+
   
   module.exports = {
-    dummy
+    dummy,totalLikes
   }
