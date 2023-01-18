9a7a851f2b273107034ac56758849f3669284f77
HorusLiang <yongqing.liang@outlook.com>
2023-01-18 22:16:58 +0100

    6.5: anecdotes, step3

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -16,11 +16,11 @@ const App = () => {
     event.target.anecdote.value = ''
     dispatch(createAnecdote(content))
   }
-
+  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
